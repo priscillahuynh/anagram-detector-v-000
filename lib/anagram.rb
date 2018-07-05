323ee868fa68b97d863b8b8ee52abf10ee29bba4
Priscilla H <priscillamhuynh@gmail.com>
2018-07-05 05:28:23 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -7,7 +7,7 @@ class Anagram
   end
 
   def match(array_of_words)
-    array_of_words.select {|word|word.split("").sort == word}
+    array_of_words.select {|word|word.split("").sort == word.split("").sort}
 
   end
 end
